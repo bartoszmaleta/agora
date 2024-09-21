0713ba9eb4eab7639889c0e6f5582c363ffd728b
bartoszmaleta <bartosz.maleta@gmail.com>
2024-09-21 20:49:15 +0200

    [ADD] fields

---
+++ src/main/java/com/agora/model/Post.java
@@ -0,0 +1,10 @@
+package com.agora.model;
+
+import java.time.LocalDate;
+
+public class Post {
+    private long id;
+    private String title;
+    private String content;
+    private LocalDate createdAt;
+}
